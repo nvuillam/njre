70a605bbd6041a09bbd97e827c8ad969dc34f77f
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-13 16:09:22 -0400

    Fixed install + tests + linter

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+const njre = require('..')
+
+describe('Install', () => {
+  it('should install JRE with default options without throwing an error', () => {
+    return njre.install()
+  }).timeout(100000)
+
+  it('should install JRE with custom options without throwing an error', () => {
+    return njre.install(11, { os: 'aix', arch: 'ppc64' })
+  }).timeout(100000)
+})
