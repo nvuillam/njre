554189079cd2f4d6454c36930e90b49f2029f10e
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-13 16:12:04 -0400

    Use custom implementation in test

--- tests/test.js
+++ tests/test.js
@@ -6,6 +6,6 @@ describe('Install', () => {
   }).timeout(100000)
 
   it('should install JRE with custom options without throwing an error', () => {
-    return njre.install(11, { os: 'aix', arch: 'ppc64' })
+    return njre.install(11, { os: 'aix', arch: 'ppc64', openjdk_impl: 'openj9' })
   }).timeout(100000)
 })
