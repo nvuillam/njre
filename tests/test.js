2d432120b8b16032e78b06d85d123a9e94ef4200
Mark Heiges <mark.heiges@semanticbits.com>
2023-09-15 14:41:56 -0400

    migrate to adoptium API v3; fixes #15

--- tests/test.js
+++ tests/test.js
@@ -7,7 +7,11 @@ describe('Install', () => {
     return njre.install()
   }).timeout(100000)
 
-  it('should install JRE with custom options without throwing an error', () => {
-    return njre.install(11, { os: 'aix', arch: 'ppc64', openjdk_impl: 'openj9' })
+  it('should install JDK with custom options without throwing an error', () => {
+    return njre.install(11, { os: 'aix', arch: 'ppc64', openjdk_impl: 'hotspot', type: 'jdk' })
+  }).timeout(100000)
+
+  it('should install JDK with custom release without throwing an error', () => {
+    return njre.install(null, { release: 'jdk-21+34-ea-beta' })
   }).timeout(100000)
 })
